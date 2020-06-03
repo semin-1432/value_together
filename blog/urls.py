53f211e433e0b533ff4f0356871b8b1a38e75d6a
semin-1432 <you@example.com>
2020-06-04 03:02:55 +0900

    Added view and template by djangogirls

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # path('post/new', views.post_new, name='post_new'),
+    # path('blog/writing_page/', views.writing_page, name='writing_page'),
+    # path('blog/create/',views.create, name='create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
+# from .forms import PostForm
 # Create your views here.
 def post_list(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+# def post_new(request):
+#     form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
+
+def writing_page(request):
+    # posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/writing_page.html')
+
+# def creat(request):
+#     blog=Blog()
+#     blog.title=request.GET['title']
+#     blog.body=request.GET['body']
+#     blog.price=request.GET['price']
+#     blog.put_date=timezone.datetime.now()
+#     blog.save()
+#     return redirect('/blog/'+str(blog.id))
