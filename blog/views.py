7ea99deba678c89e3e2c790d1fffbf81a2a8cee2
semin-1432 <you@example.com>
2020-06-09 23:00:33 +0900

    djangogirls llllllll

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-#from django.shortcuts import render
+
+from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
@@ -27,19 +28,25 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
 
-# def online(request):
-#     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-#     return render(request, 'blog/online.html', {'post': post})
-#
-# def daily_necessity(request):
-#     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-#     return render(request, 'blog/daily_necessity.html', {'post': post})
-#
-# def eventitem(request):
-#     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-#     return render(request, 'blog/eventitem.html', {'post': post})
-#
-# def ingredients(request):
-#     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-#     return render(request, 'blog/ingredients.html', {'post': post})
+# def creat(request):
+#     blog=Blog()
+#     blog.title=request.GET['title']
+#     blog.body=request.GET['body']
+#     blog.price=request.GET['price']
+#     blog.put_date=timezone.datetime.now()
+#     blog.save()
+#     return redirect('/blog/'+str(blog.id))
