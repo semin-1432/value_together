d13f0351b54acdff89c16b6c9f934000d7266e11
semin-1432 <you@example.com>
2020-06-12 21:48:12 +0900

    글쓰고 보여주기 고침

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.shortcuts import redirect
 
 # from .forms import PostForm
 # Create your views here.
@@ -39,8 +40,7 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            # return redirect('post_detail', pk=post.pk)
-            return redirect('post_list')
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
